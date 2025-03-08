2965e4ab46dc4bed3d799d3aa2c844415e24b3fa
Cyril-CRGB brom <crg.bromberger@gmail.com>
2025-03-08 06:35:49 +0000

    Adding: widgets and dataPP5_cleaned_10k.csv for page predictions

--- app_pages/predictions.py
+++ app_pages/predictions.py
@@ -16,12 +16,15 @@ sns.set_style('dark')
 
 def predictions_body():
 
-    # load cluster analysis files
+    #load data
+    ## df = load_original_data()
+    ## dfcca = load_crime_committed_analyses()
+    ## dfcleanedshort = load_cleaned_data_short()
+
+    #load cluster analysis files
     version = 'v1'
-    # cluster_features = (data...
-    #                        .columns
-    #                        .to_list()
-    #                        )
+    ## cluster_features = (dfcleanedshort.columns.to_list())
+    cluster_features_testing = (pd.read_csv(f"outputs/datasets/datacleaned/{version}/dataPP5_cleaned_10k.csv").columns.to_list())
     cluster_pipeline = load_pkl_file(f"outputs/ml_pipeline/cluster_analysis/{version}/LuxuriusCluster.pkl")
     cluster_profile = pd.read_csv(f"outputs/datasets/other/{version}/clusters_profile.csv")
 
@@ -32,35 +35,38 @@ def predictions_body():
     st.write("---")
 
     # Generate Live Data
-    # check_variables_for_UI(cluster_features)
+    check_variables_for_UI(cluster_features_testing)
     X_live = DrawInputsWidgets()
 
     # Predict on live data
-    if st.button("Predict"):
-        predict_cluster(X_Live, cluster_features, cluster_pipeline, cluster_profile)
+    if st.button("Make Prediction"):
+        predict_cluster(X_Live, cluster_features_testing, cluster_pipeline, cluster_profile)
 
 
-def check_variables_for_UI(cluster_features):
+def check_variables_for_UI(cluster_features_testing):
     import itertools
     # The widgets inputs are the features used in the pipeline
     # We combine them only with unique values
-    combines_features = set(
+    combined_features = set(
         list(
-            itertools.chain(cluster_features)
+            itertools.chain(cluster_features_testing) #need to get rid of '_testing' befor going into PRODUCTION
         )
     )
-    st.write(f"* There are {len(combined_features)} features for the UI: \n\n {combines_features}"
+    st.write(f"* There are {len(combined_features)} features for the UI: \n\n {combined_features}"
     )
 
 def DrawInputsWidgets():
 
     #load data
-    df = load_original_data()
-    dfcca = load_crime_committed_analyses()
-    dfcleanedshort = load_cleaned_data_short()
+    ## df = load_original_data()
+    ## dfcca = load_crime_committed_analyses()
+    ## dfcleanedshort = load_cleaned_data_short()
+    version = 'v1'
+    dftesting = pd.read_csv(f"outputs/datasets/datacleaned/{version}/dataPP5_cleaned_10k.csv")
 
-    # Creating input widgets only for 4 features
-    col1, col2, col3, col4 = st.beta_columns(4)
+    # Creating input widgets for 5 features
+    col1, col2, col3 = st.beta_columns(3)
+    col4, col5 = st.beta_columns(2)
 
     #Using the features to feed the ML Pipeline -> values from check_variables_for_UI() result
 
@@ -72,13 +78,56 @@ def DrawInputsWidgets():
     # from here on we draw the widget based on the variable type (numerical or categorical)
     # and set initial values
     with col1:
-        feature = "vict_sex"
+        feature = "Vict Sex"
         st_widget = st.selectbox(
-            label=feature,
-            options=dfcleanedshort[feature].unique()
+            label="Sex or Gender",
+            options=dftesting[feature].unique()
         )
     X_live[feature] = st_widget
 
+    with col2:
+        feature_cd = "Weapon Used Cd" # the value we want to store
+        feature_desc = "Weapon Desc" # the value we want to display
+        # drop missing values and create a mapping dictionary
+        weapon_mapping = dftesting.dropna(subset=[feature_cd, feature_desc]).drop_duplicates(subset=[feature_cd, feature_desc]).set_index(feature_desc)[feature_cd].to_dict()
+        # show descriptions (Weapon Desc) in the dropdown
+        widget_desc = st.selectbox(
+            label="Weapon ownership or regularly seen",
+            options=list(weapon_mapping.keys()) # display only weapon descriptions
+        )
+        # convert back to the corresponding weapon Used Cd
+        widget_cd = weapon_mapping[widget_desc]
+    X_live[feature] = widget_cd
 
+    with col3:
+        feature = "Vict Age"
+        st_widget = st.number_input(
+            label="Age",
+            min_value=int(0),
+            max_value=int(dftesting[feature].max()),
+            value=int(dftesting[feature].median()),
+            step=1
+        )
+    X_live[feature] = st_widget
+
+    with col4:
+        feature = "Premis Desc"
+        st_widget = st.selectbox(
+            label="Location most often visited",
+            options=dftesting[feature].unique()
+        )
+    X_live[feature] = st_widget
+
+    with col5:
+        feature = "Amount"
+        st_widget = st.selectbox(
+            label="Value in $ of the good at risk",
+            options=dftesting[feature].unique()
+        )
+    X_live[feature] = st_widget
+
+    st.write(X_live)
+
+    return X_live
 
 
