f2594c36bfddea79cb7e5b85c3396b36215b8062
Cyril-CRGB brom <crg.bromberger@gmail.com>
2025-03-12 18:27:50 +0000

    Fixing: requirement issue new version of feature-engine not working

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ app.add_page("Study", study_body)
 app.add_page("Hypothesis & Validation", hypothesis_body)
 app.add_page("ML Cluster", cluster_body)
 #for prod
-#app.add_page("Predictions prod", predictions_body_prod)
+app.add_page("Predictions prod", predictions_body_prod)
 #for testing
-app.add_page("Predictions testing", predictions_body_test)
+#app.add_page("Predictions testing", predictions_body_test)
 
 
 
